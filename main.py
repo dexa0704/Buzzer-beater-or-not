9449b00ac69cd81d659e58b5503abbbca029f245
Dejan Zaric <dexa0704@gmail.com>
2018-05-16 01:01:12 +0200

    Almost complete first half of project.
    -Crop imageforsemaphore part with circle
    -Detect contours and make a conclusion
    
    Not applicable for all angles and images ...

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 import cv2
 import numpy as np
-import neuralNetwork
+import neuralNetwork as nn
+from matplotlib import pyplot as plt
 
 
-def checkShotTime(imgPath):
+def shotClockViolation(imgPath):
     '''
 
-    Function for checking time on table semaphore. If zero on table, shot isn't valid
-    Number recognitions with MNIST
+    Function for checking time on table semaphore. If zero on table, shot isn't valid. Checking specific circle on the
+    semaphore. If that circle exist clock is on zero, if circle doesn't exist time still counting ...
+
 
     :param imgPath: relative path for image for checking time on table
-    :return: boolen represent success, true -> valid shot, false -> invalid
+    :return: boolen represent presents of violation, true -> there is a violation, false -> no violation
 
     '''
 
+    im = cv2.imread('../../oneSecLeft.jpg')
+    print(im.shape)
+    # prva koord je Y a druga X, X ide lepo sa leva na desno od 0.0, a y ide odozgo na dole 0,0 mu je gore !
+    crop_img = im[870:1000, 1228:1450] #- ovo je za semaphoe four.. 3:09 ta tura slika
+    #crop_img = im[980:1100, 1128:1300] # - ovo je sa 5:57 tura slika
 
+    plt.imshow(crop_img, interpolation='bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
 
-    '''
-    results = mnist(imgPath)
-    if results[-1] == 0 
+    bilateral_filtered_image = cv2.bilateralFilter(crop_img, 5, 175, 175)
+
+    gray = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+
+    edge_detected_image = cv2.Canny(gray, 75, 200)
+    cv2.imshow('Edge', edge_detected_image)
+    cv2.waitKey(0)
+
+    rows = gray.shape[0]
+
+    _, contours, hierarchy = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #circles = cv2.HoughCircles(edge_detected_image, cv2.HOUGH_GRADIENT, 1, rows / 8, param1=100, param2=30, minRadius=1, maxRadius=30)
+
+    #print(circles)
+    #print(len(contours))
+
+    if len(contours) != 0:
+        print("\n\nShot clock violation !!! \n\n")
+        return True
+    else:
+        print("\n\nStill have time...\n\n")
         return False
-    '''
-    print(imgPath)
-
-    return True
 
 
 def checkBallHandContact(imgPath):
@@ -31,12 +56,11 @@ def checkBallHandContact(imgPath):
     Function for detect ball on picture and try to find contact between ball and players hand
     Detection with open cv and numpy
 
-    :param imgPath: relative path for image for checking time on table
-    :return: boolen represent contact between hand and ball, true -> with contact, false -> without contact
+    :param imgPath: relative path for image for checking ball contact with hand
+    :return: boolean represent contact between hand and ball, true -> with contact, false -> without contact
     '''
 
-    im = cv2.imread('../../test1.jpg')
-
+    '''
     # convert to HSV space
     im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
     # take only the orange, highly saturated, and bright parts
@@ -60,21 +84,25 @@ def checkBallHandContact(imgPath):
 
     out = np.vstack([im_orange, im])
     cv2.imwrite('out.png', out)
+    '''
 
-    print(imgPath)
+    #print(imgPath)
 
-    return False
+    return True
 
 
 if __name__ == "__main__":
+
     print("Welcome to buzzer beater application :)\n\n")
 
     path = "dejan" #input("Insert relative image path for validation: ")
 
     contact = checkBallHandContact(path)
-    tableSemaphore = checkShotTime(path)
+    violation = shotClockViolation(path)
 
-    if contact is False or (contact is True and tableSemaphore is True):
-        print("\n\nThis is VALID shot !")
-    else:
+    if contact is False:
+        print("\n\nThis is VALID shot :) !")
+    elif violation is True:
         print("\n\nThis shot is INVALID !!!")
+    else:
+        print("\n\nThis is VALID shot :) !")
